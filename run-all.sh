bde612cfff151376e76389dfb3c30a1b432a6d8e
Michał Woś <mw792829570@gmail.com>
2015-03-19 11:11:34 +0100

    run all sh

--- run-all.sh
+++ run-all.sh
@@ -2,29 +2,29 @@ mvn clean install
 
 # put data into hdfs
 rm sample-input/*~
+hdfs dfs -mkdir -p /user/mwos
 hdfs dfs -rm -r -f /user/mwos/sample-input
 hdfs dfs -put sample-input /user/mwos/sample-input
 hdfs dfs -rm -r -f /user/mwos/result-*
 
-
 # run
-#hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Union /user/mwos/sample-input /user/mwos/result-union/
+hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Union /user/mwos/sample-input /user/mwos/result-union/
 echo "UNION: "
 hdfs dfs -cat /user/mwos/result-union/part*
 
-#hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Intersection /user/mwos/sample-input /user/mwos/result-intersection/
+hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Intersection /user/mwos/sample-input /user/mwos/result-intersection/
 echo "INTERSECTION: "
 hdfs dfs -cat /user/mwos/result-intersection/part*
 
-#hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Difference /user/mwos/sample-input /user/mwos/result-difference/
+hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Difference /user/mwos/sample-input /user/mwos/result-difference/
 echo "DIFFERENCE: "
 hdfs dfs -cat /user/mwos/result-difference/part*
 
-#hadoop jar target/map-reduce-1.0-SNAPSHOT.jar FirstColumnProjection /user/mwos/sample-input /user/mwos/result-projection/
+hadoop jar target/map-reduce-1.0-SNAPSHOT.jar FirstColumnProjection /user/mwos/sample-input /user/mwos/result-projection/
 echo "PROJECTION (1st element): "
 hdfs dfs -cat /user/mwos/result-projection/part*
 
-#hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Select /user/mwos/sample-input /user/mwos/result-select/
+hadoop jar target/map-reduce-1.0-SNAPSHOT.jar Select /user/mwos/sample-input /user/mwos/result-select/
 echo "SELECT (1st element == 'cc'): "
 hdfs dfs -cat /user/mwos/result-select/part*
 
